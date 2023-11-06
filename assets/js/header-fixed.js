8ab6fcc2bcadb985d38556d3c18a9a96c615b6a8
tonGuedesDev <ofcontatowellington@gmail.com>
2023-11-05 22:53:23 -0300

    versão mobile concluida

--- assets/js/header-fixed.js
+++ assets/js/header-fixed.js
@@ -7,7 +7,7 @@ window.addEventListener('scroll', function() {
     if (posicaoScrollPagina > 20) {
         header.style.backgroundColor = "#fff"
         header.style.boxShadow = "0 1px 2px 0 rgb(0 0 0 / 0.05)"
-        header.style.transition = "all 1s ease"
+        header.style.transition = "box-shadow 1s ease"
     } else {
         header.style.backgroundColor = "#fff"
         header.style.boxShadow = "none"
